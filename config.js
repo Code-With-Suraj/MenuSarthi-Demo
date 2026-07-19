d2cf8367776cb59b9460d5cc4a716eef3b47a7b2
suraj singh <surajsingh.noida98@gmail.com>
2026-07-19 16:21:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
 const CONFIG = {
   // Replace this with your Google Apps Script Web App Deployment URL
   // Example: "https://script.google.com/macros/s/.../exec"
-  GAS_API_URL: "https://script.google.com/macros/s/AKfycbw25OZxxnT0YdABP2J-9EQAr0EHMiOecrppDOfdQYQo2DMZluxHAGZOGVRX1tHBWRoKhA/exec",
+  GAS_API_URL: "https://script.google.com/macros/s/AKfycbyc7GHEXqBUY0a4sv0Fejd8pOMMjjCjvZlvu8q4kEz3d34nOU0PnMQl-jXLWFH8XSBv_w/exec",
   REQUEST_TIMEOUT: 30000
 };
